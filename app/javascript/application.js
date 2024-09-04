8c0a255f63ec8cad66e0ecddcf08dfe09718e56f
Aima089 <aimarubab123@gmail.com>
2024-09-04 15:41:42 +0500

    Your commit message here

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,9 @@
 import { Application } from "@hotwired/stimulus"
+import Rails from "@rails/ujs";
 
+// Start Stimulus application
 const application = Application.start()
 export { application }
+
+// Start Rails UJS
+Rails.start()
